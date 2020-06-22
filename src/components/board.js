d3a83f5c54b0fb30ef033b2f30bea3698f926d79
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-06-22 11:51:30 +0100

    styled board

--- src/components/board.js
+++ src/components/board.js
@@ -6,14 +6,29 @@ function Board() {
     test.push(i);
   }
   return (
-    <div>
-      <table style={{ margin: "1em o" }}>
+    <div style={{ display: "flex", justifyContent: "center" }}>
+      <table
+        style={{
+          display: "flex",
+          margin: "1em 0",
+          borderSpacing: 0,
+          borderCollapse: "collapse",
+        }}
+      >
         <tbody>
           {test.map((row, i) => (
             <tr key={i}>
               {test.map((cell, j) => (
-                <td key={j} style={{ backgroundColor: "dodgerblue" }}>
-                  {cell}
+                <td
+                  key={j}
+                  style={{
+                    border: "1px solid red",
+                    height: "1.5em",
+                    width: "1.5em",
+                    // backgroundColor: "dodgerblue",
+                  }}
+                >
+                  {null}
                 </td>
               ))}
             </tr>
